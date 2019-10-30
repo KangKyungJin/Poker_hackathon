e85d777e08ed2bc864b3c291f9057901b77bcd7d
j.kang <kjinn62@gmail.com>
2019-10-30 16:03:12 -0700

    working player/dealer classes

--- cards.py
+++ cards.py
@@ -1,36 +1,28 @@
 import random
 #class card to create card objects
 class card:
-    def __init__(self,suit, val, numericval):
+    def __init__(self,suit, val, numericVal):
         self.suit=suit
         self.val= val
-        self.numericval=numericval
-    
+        self.numericVal=numericVal
     def displaycardinfo(self):
-        print(self.suit,self.val) 
-        return self 
+        return(f"{self.val}{self.suit}")
+    def returnVal(self):
+        return(self.numericVal)
 
 #ace class to apply unique property of Ace's bineg 1 or 11
 class ace:
     def __init__(self,suit, val):
         self.suit=suit
         self.val= val
-        self.numericval=None
-    def acevalue(self,handval):
-        if handval>10:
-            self.numericval=1
-        if handval<10:
-            self.numericval=11
-# class hand:
-#     def __init__(self,numofcards=0):
-#         self.totalval=0
-#         self.numofcards=numofcards
-#     def addcard(self):
-#         self.numofcards+=1
-#     def handtotal(self):
-#         self.totalval+=self.numofcards.numericval
+        self.numericVal=None
+    def acevalue(self,handVal):
+        if handVal>10:
+            self.numericVal=1
+        if handVal<10:
+            self.numericVal=11
 suitdict={ '0': '♥', '1':'♦', '2':'♠', '3': '♣'}
-carddict={'1':'2','2':'3','3':'4','4':'5','5':'6','6':'7','7':'8','8':'9', '9':'10', '10':'jack', '11':'queen','12':'king','13':'ace'}
+carddict={'1':'2','2':'3','3':'4','4':'5','5':'6','6':'7','7':'8','8':'9', '9':'10', '10':'Jack', '11':'Queen','12':'King','13':'Ace'}
 
 #deck class for building a deck
 class Deck:
@@ -39,24 +31,14 @@ class Deck:
         for d in range(deckAmmount):
             for x in range(4):
                 for y in range(13):
-                    self.deck.append(card(x,y+1,y+1))
-
+                    self.deck.append(card(x,y+1,y+2))
 
     def shuffles(self):
         length=len(self.deck)
-        
         for i in range(length): 
             r = random.randint(0,length-1) 
             self.deck[i], self.deck[r] = self.deck[r], self.deck[i]
-            
         return self
-    
     def draw(self):
         card = self.deck.pop()
         return card
-
-deckOne = Deck(4)                   
-
-for x in range(0,len(deckOne.deck)):
-    deckOne.deck[x].suit=suitdict[str(deckOne.deck[x].suit)]
-    deckOne.deck[x].val=carddict[str(deckOne.deck[x].val)]
--- player.py
+++ player.py
@@ -1,13 +1,23 @@
-from cards import Deck, card
+from cards import *
 deck = Deck(4) 
 deck = deck.shuffles()
+for x in range(0,len(deck.deck)):
+    deck.deck[x].suit=suitdict[str(deck.deck[x].suit)]
+    deck.deck[x].val=carddict[str(deck.deck[x].val)]
 class player:
     def __init__ (self):
         self.hand=[deck.draw(),deck.draw()]
-
-    
     def playerHit(self):
         self.hand.append(deck.draw())
-erik=player()        
-print(erik.hand[0].displaycardinfo())
+        return self
 
+class dealer:
+    def __init__(self):
+        self.hand=[deck.draw(),deck.draw()]
+    def playerHit(self):
+        self.hand.append(deck.draw())
+        return self
+erik=player()        
+
+print(erik.playerHit().hand[1].displaycardinfo())
+print(erik.hand[1].returnVal())
